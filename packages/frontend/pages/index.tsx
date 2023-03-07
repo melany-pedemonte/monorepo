9615476716d5ab097afe10ad8b8dca1ecd3412b2
AlejanraXside <apizzi@xsidesolutions.com>
2023-03-07 15:08:32 -0300

    feat(frontend): Add theme styles and components

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,25 +1,31 @@
-import styles from './index.module.scss';
+import { Container } from "@mui/material";
+import Dashboards from "components/landingPage/Dashboards";
+import Ecommerce from "components/landingPage/Ecommerce";
+import Features from "components/landingPage/Features";
+import Footer from "components/landingPage/Footer";
+import GetStarted from "components/landingPage/GetStarted";
+import Hero from "components/landingPage/Hero";
+import Screens from "components/landingPage/Screens";
+import Sections from "components/landingPage/Sections";
+import { NextPage } from "next";
+import { Fragment } from "react";
 
-export function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.scss file.
-   */
+const Home: NextPage = () => {
   return (
-    <div className={styles.page}>
-      <div className="wrapper">
-        <div className="container">
-          <div id="welcome">
-            <h1>
-              <span> Mi proyecto react, </span>
-              Welcome frontend 👋
-            </h1>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+    <Fragment>
+      <Hero />
 
-export default Index;
+      <Container>
+        <Features />
+        <Dashboards />
+        <Screens />
+        <Ecommerce />
+        <Sections />
+        <GetStarted />
+        <Footer />
+      </Container>
+    </Fragment>
+  );
+};
+
+export default Home;
---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,44 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { StyledEngineProvider } from "@mui/material/styles";
+import { FC, ReactNode } from "react";
+import { Toaster } from "react-hot-toast";
+import RTL from "./components/RTL";
+import useSettings from "./hooks/useSettings";
+import { ukoTheme } from "./theme";
+
+type AppProps = {
+  children: ReactNode;
+};
+
+const App: FC<AppProps> = ({ children }) => {
+  const { settings } = useSettings();
+
+  // App theme
+  const appTheme = ukoTheme({
+    theme: settings.theme,
+    direction: settings.direction,
+    responsiveFontSizes: settings.responsiveFontSizes,
+  });
+
+  // toaster options
+  const toasterOptions = {
+    style: {
+      fontWeight: 500,
+      fontFamily: "'Montserrat', sans-serif",
+    },
+  };
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={appTheme}>
+        <RTL direction={appTheme.direction}>
+          <CssBaseline />
+          <Toaster toastOptions={toasterOptions} />
+          {children}
+        </RTL>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+};
+
+export default App;
